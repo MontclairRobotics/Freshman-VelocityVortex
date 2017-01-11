3b802576448bbd5fa78d25c7da9bf6e51c92956b
RickHarrisonBaldOverlord <garrettburroughs02@gmail.com>
2017-01-11 15:25:33 -0500

    Added intake stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -11,17 +11,36 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 @TeleOp
 public class teleop extends OpMode{
     DriveTrain driveTrain;
+    Intake intake;
     Controller controller;
     @Override
     public void init() {
         telemetry.addData("INFO","Initialized");
         driveTrain = new DriveTrain();
         controller = new Controller();
+        intake = new Intake();
         Controller.init(gamepad1, gamepad2);
     }
 
     @Override
     public void loop() {
         driveTrain.setDriveTank(controller.getRightPower(), controller.getLeftPower());
+        boolean intaking = false;
+        if (controller.getButtonPressed("Y")){
+            intake.intakeDown();
+        }else if(intaking){
+            intaking = true;
+            intake.intakeUp();
+        }else{
+            intake.intakeReg();
+        }
+        if (controller.dpad("UP")) {
+            intake.incrUp(10);
+            telemetry.addData("INFO", "Intake Position " + intake.getVals());
+        }
+        if (controller.dpad("Down")) {
+            intake.incrDown(10);
+            telemetry.addData("INFO", "Intake Position " + intake.getVals());
+        }
     }
 }
