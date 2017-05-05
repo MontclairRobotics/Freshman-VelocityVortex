0a70ab35bdb3ff0383830ba2f61a38a23b5b1e3f
H1PPX <willcty@gmail.com>
2017-05-05 14:54:00 -0400

    add DIM led test and lib code in the gradle build script

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTestLedDIM.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+
+/**
+ * Created by willc on 5/5/2017.
+ */
+
+//DIM refers to Device Interface Module otherwise known as Core Interface Module
+
+public class AutoTestLedDIM extends AutoMode{
+    @Override
+    public void init() {
+        setState(0);
+        autoInit();
+    }
+
+    //DIM setup
+    private static final int    BLUE_LED    = 0;     // Blue LED channel on DIM
+    private static final int    RED_LED     = 1;     // Red LED Channel on DIM
+    DeviceInterfaceModule dim;
+
+    @Override
+    public void loop() {
+        switch(state){
+            case 1:
+                dim.setLED(BLUE_LED,true);
+                dim.setLED(RED_LED,true);
+                telemetry.addData("INFO","Blue and Red LEDs true");
+                break;
+
+            case 2:
+
+                break;
+        }
+    }
+}
